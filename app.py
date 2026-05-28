70f0b565fc9bfb4038a2aea824b1fd4f958752b9
zxc545477 <shit20031204@gmail.com>
2026-05-28 10:42:10 +0000

    render fix

--- app.py
+++ app.py
@@ -182,4 +182,6 @@ def predict_video_route():
     }), 501
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
